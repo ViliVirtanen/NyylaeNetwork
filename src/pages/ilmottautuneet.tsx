aaea7cd025467261b11c10dfa9b6e8bcf0b5c5de
enoquemaculuba <96523554+enoquemaculuba@users.noreply.github.com>
2023-05-15 19:27:10 +0300

    Added registrations

--- src/pages/ilmottautuneet.tsx
+++ src/pages/ilmottautuneet.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import { Registration } from "@/types";
+import React, { useEffect, useState } from "react";
 
 const Ilmottautuneet = () => {
+  const [registered, setRegistered] = useState<Registration[]>([]);
+
+  useEffect(() => {
+    const registrations = localStorage.getItem("registrations");
+    if (registrations) {
+      setRegistered(JSON.parse(registrations));
+    }
+  }, []);
   return <div>Ilmottautuneet</div>;
 };
 
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export type Registration = {
+    name: string;
+    email: string;
+}
