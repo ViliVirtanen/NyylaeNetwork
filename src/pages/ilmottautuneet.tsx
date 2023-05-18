e44ec474ba3c449558be6fc074065b77f7649fee
aholas <lassi.aho29@gmail.com>
2023-05-18 22:08:50 +0300

    create the page for registered users

--- src/pages/ilmottautuneet.tsx
+++ src/pages/ilmottautuneet.tsx
@@ -10,7 +10,26 @@ const Ilmottautuneet = () => {
       setRegistered(JSON.parse(registrations));
     }
   }, []);
-  return <div>Ilmottautuneet</div>;
+
+  return (
+    <div
+      id="registered" className={`flex min-h-screen flex-col items-center py-24 bg-red-`}
+    >
+      <h2 style={{ fontSize: "3rem", paddingTop: "4rem" }}>Nyyläperhe</h2>
+      <table id="list">
+        <tr id="row">
+          <th className="number"></th>
+          <th className="name">Nimi</th>
+        </tr>
+        {registered.map((c, i) => (
+          <tr>
+            <td className="number">{i + 1}</td>
+            <td className="name">{c.name}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  )
 };
 
 export default Ilmottautuneet;
